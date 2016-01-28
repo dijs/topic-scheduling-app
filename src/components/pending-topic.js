c8eaf4f6b0d5e5f596ef6167d369e79aed653606
Richard van der Dys <richard.vanderdys@gmail.com>
2016-01-28 13:29:16 -0600

    Changed to votes

--- src/components/pending-topic.js
+++ src/components/pending-topic.js
@@ -6,6 +6,6 @@ export default function Topic({title, score = 0, upvote, downvote, remove}) {
     <span className='pull-right fa fa-trash' onClick={() => remove(title)}></span>
     <span className='vote pull-right' onClick={() => downvote(title)}>⬇&nbsp;&nbsp;</span>
     <span className='vote pull-right' onClick={() => upvote(title)}>⬆&nbsp;&nbsp;</span>
-    <span className='pull-right'>{score} pts&nbsp;&nbsp;&nbsp;</span>
+    <span className='pull-right'>{score} votes&nbsp;&nbsp;&nbsp;</span>
   </div>
 }
